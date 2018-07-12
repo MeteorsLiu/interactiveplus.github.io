3784f4926a540b58ae76d286dc1a093c4860921d
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-07-11 21:37:58 -0400

    Code Review

--- README.md
+++ README.md
@@ -1,41 +1,30 @@
-New Website
+#New Website
+Basically a new website design for Interactive+
+
+## Authors
+Create by Meteoroid
+Code Review by Windy
 
 
-
-Create by LiuXing
-
-
-
-Update:
-
-
+## Update:
 2018.7.9：
-Header，Banner，Section-One完工
+- [x] \(index.html\)Header，Banner，Section-One完工
+- [x] \(index.html\)同时将布局策略改为Flex和padding和margin，放弃传统position
 
-同时将布局策略改为Flex和padding和margin，放弃传统position
-
-
-
-
-
-
-
-
-
+---
 
 2018.7.10：
-完善Navbar，Section-One对手机的兼容
+- [x] \(index.html\)完善Navbar，Section-One对手机的兼容
+- [x] \(index.html\)Section-Two整体设计完工，但未实现对手机的兼容
 
-Section-Two整体设计完工，但未实现对手机的兼容
-
-
-
-
-
-
-
-
-
+---
 
 2018.7.11:
-完善Section-Two对手机的兼容
+- [x] \(index.html\)完善Section-Two对手机的兼容
+
+---
+
+2018.7.12 09:32
+- [x] \(index.html\) Code-Review \(确保代码缩进无误\)
+- [x] \(index.html\) 页面加载速度优化\(CSS/JS Preload\)
+- [x] \(index.html\) 修复 Section1 背景图片在部分手机上显示超出页面范围的Bug
--- js/main.js
+++ js/main.js
@@ -1,46 +1,43 @@
-$(document).ready(function() {
-
-   window.cookieconsent.initialise({
-                                  'palette': {
-                                      'popup': {
-                                        'background': '#efefef',
-                                        'text': '#404040'
-                                      },
-                      'button': {
-                      'background': '#1f1915',
-                        'text': '#ffffff'
-                      }
-                                  },
-                                  'theme': 'edgeless',
-                                    'position': 'bottom',
-                                    'content': {
-                                        'message': '此网站需要使用Cookie，以便于我们可以给您更好的访问体验',
-                                        'dismiss': '我知道了',                                                                                
-                                    }
-                })
-
- var mySwiper = new Swiper ('#Banner', {
-    autoplay: true,
-    loop: true,
-    
-
-    navigation: {
-      nextEl: '#Banner-Button-Next',
-      prevEl: '#Banner-Button-Prev',
-    },
-    
-  })  
-
-   if(mySwiper.slides.length = 1){
-    mySwiper.destroy();
-  }      
-
- });
-$('#English').click(function() {
-  $.cookie('lang','en');
+$(document).on("load",function() { //$().load function is deprecated
+	"use strict";
+	if($.cookie("XSYD_CookiePrompted") === null){
+		if($.cookie("XSYD_Language") === null){
+			$.cookie("XSYD_Language","cn"); //Cookie is supposed to be set at the language detection page.
+		}
+		var CookieConsent = {message:"", dismiss:""};
+		if($.cookie("XSYD_Language") === "cn"){
+			CookieConsent.message = "此网站需要使用Cookie，以便于我们可以给您更好的访问体验";
+			CookieConsent.dismiss = "我知道了";
+		}else if($.cookie("XSYD_Language") === "en"){
+			CookieConsent.message = "This website needs your cookie in order for us to give you a better experience";
+			CookieConsent.dismiss = "Got it";
+		}
+		window.cookieconsent.initialise(
+			{
+				palette: {
+					popup: {
+						background: '#efefef',
+						text: '#404040'
+					 },
+					 button: {
+						background: '#1f1915',
+						text: '#ffffff'
+					 }
+				},
+				theme: 'edgeless',
+				position: 'bottom',
+				content: CookieConsent
+			}
+		);
+	}
 });
-$('#Chinese').click(function() {
-  $.cookie('lang','zh');
+$('#LangSelection_English').click(function() {
+	"use strict";
+	$.cookie('XSYD_Language','en');
+});
+$('#LangSelection_Chinese').click(function() {
+	"use strict";
+	$.cookie('XSYD_Language','cn');
 });
 
 
