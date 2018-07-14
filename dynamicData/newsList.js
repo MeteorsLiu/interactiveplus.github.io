f63f8d18acb214e7ed71dcd2734d67e1c6297c0b
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-07-13 23:50:15 -0400

    Update Newsdata

--- dynamicData/newsList.js
+++ dynamicData/newsList.js
@@ -4,15 +4,15 @@ var XSYD_News = {
             cn:{
                 title: "形随意动简要介绍",
                 briefDescription: "本文章介绍形随意动的前身由来, 我们做什么和我们未来要做什么!",
-                articleURL: "0_InterActivePlusIntro_CN.html",
-                showOnIndexNews: false, //Not showing on index news block because has been added to slides.
+                articleURL: " 0_InteractivePlusIntro_CN.html",
+                showOnIndexNews: false, //Not showing on index news block because this is the team introduction.
                 isURLAbsolute: false, //if URL is not absolute, articleBaseURL will be added.
                 ArthorID: 0
             },
             en:{
                 title: "InterActive+ Introduction",
                 briefDescription: "This article describes how Interactive+ were founded earlier, what we do now and what we are going to do in the future!",
-                articleURL: "0_InterActivePlusIntro_EN.html",
+                articleURL: " 0_InteractivePlusIntro_EN.html",
                 showOnIndexNews: false,
                 isURLAbsolute: false,
                 AuthorID: 0
