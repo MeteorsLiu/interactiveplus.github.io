0732d6464612e9f204cf1aeed2d178c242821613
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-07-13 23:55:51 -0400

    News List URL Fix

--- dynamicData/newsList.js
+++ dynamicData/newsList.js
@@ -4,7 +4,7 @@ var XSYD_News = {
             cn:{
                 title: "形随意动简要介绍",
                 briefDescription: "本文章介绍形随意动的前身由来, 我们做什么和我们未来要做什么!",
-                articleURL: " 0_InteractivePlusIntro_CN.html",
+                articleURL: "0_InteractivePlusIntro_CN.html",
                 showOnIndexNews: false, //Not showing on index news block because this is the team introduction.
                 isURLAbsolute: false, //if URL is not absolute, articleBaseURL will be added.
                 ArthorID: 0
@@ -12,7 +12,7 @@ var XSYD_News = {
             en:{
                 title: "InterActive+ Introduction",
                 briefDescription: "This article describes how Interactive+ were founded earlier, what we do now and what we are going to do in the future!",
-                articleURL: " 0_InteractivePlusIntro_EN.html",
+                articleURL: "0_InteractivePlusIntro_EN.html",
                 showOnIndexNews: false,
                 isURLAbsolute: false,
                 AuthorID: 0
