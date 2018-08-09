2dfdaa8e4252b320e34053bc8d80e03d93fdc83e
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-08-09 12:39:07 -0400

    Add Readin Promo

--- dynamicData/indexslides.js
+++ dynamicData/indexslides.js
@@ -1,6 +1,39 @@
 var XSYD_IndexSlides = {
     slides: [
         {
+            backgroundImage: "static/slidesResources/slide3.jpg",
+            cn:{
+                title: '<span class="text-black">Readin上线!</span>',
+                description: '<span class="text-black">由迪莫开发的Readin应用现在已经可以在Microsoft Store中下载</span>',
+                disableArticleButton: true,
+                customButton: true,
+                customButtonURL: "http://dimojang.com/rz/",
+                customButtonText: "前往产品详情",
+                articleIDRelated: -1
+            },
+            en:{
+                title: '<span class="text-black">Readin is now available!</span>',
+                description: '<span class="text-black">Our APP Readin developed by Dimo Jang is now availble in Microsoft Store</span>',
+                disableArticleButton: true,
+                customButton: true,
+                customButtonURL: "http://dimojang.com/rz/",
+                customButtonText: "View Product Info (Simplified Chinese)",
+                articleIDRelated: -1
+            }
+        },
+        {
+            backgroundImage: "static/slidesResources/slide2.jpg",
+            cn:{
+                title: '<span class="text-black">形随意动招新!</span>',
+                description: '<span class="text-black">我们已经准备好在2019年迎接新人了!</span>',
+                disableArticleButton: false,
+                customButton: false,
+                customButtonURL: null,
+                customButtonText: null,
+                articleIDRelated: 2
+            },
+        },
+        {
             backgroundImage: "static/slidesResources/slide1.jpg",
             cn:{
                 title: '<span class="text-white">形随意动新官网上线!</span>',
@@ -20,18 +53,6 @@ var XSYD_IndexSlides = {
                 customButtonText: null,
                 articleIDRelated: 1
             }
-        },
-        {
-            backgroundImage: "static/slidesResources/slide2.jpg",
-            cn:{
-                title: '<span class="text-black">形随意动招新!</span>',
-                description: '<span class="text-black">我们已经准备好在2019年迎接新人了!</span>',
-                disableArticleButton: false,
-                customButton: false,
-                customButtonURL: null,
-                customButtonText: null,
-                articleIDRelated: 2
-            },
         }
     ]
 }
