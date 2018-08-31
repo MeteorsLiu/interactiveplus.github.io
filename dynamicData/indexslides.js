d4942b074512c1cbaa63a7d04b156374901bab9e
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-08-31 17:09:27 -0400

    Change Readin Link

--- dynamicData/indexslides.js
+++ dynamicData/indexslides.js
@@ -4,11 +4,11 @@ var XSYD_IndexSlides = {
             backgroundImage: "static/slidesResources/slide3.jpg",
             cn:{
                 title: '<span class="text-black">Readin上线!</span>',
-                description: '<span class="text-black">由迪莫开发的Readin应用现在已经可以在Microsoft Store中下载</span>',
+                description: '<span class="text-black">由迪莫开发的Readin应用现在已经可以在Microsoft商店中下载</span>',
                 disableArticleButton: true,
                 customButton: true,
-                customButtonURL: "http://dimojang.com/rz/",
-                customButtonText: "前往产品详情",
+                customButtonURL: "https://www.microsoft.com/zh-cn/p/readin/9nx3jtgvpl27?rtc=1/",
+                customButtonText: "Microsoft商店",
                 articleIDRelated: -1
             },
             en:{
