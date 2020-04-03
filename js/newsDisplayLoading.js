370e1451cac21e2ddbd372a46e410dcf5df7287a
Windy Feng <1745500559@qq.com>
2020-04-03 09:26:24 +0800

    Optimize Article Author Info

--- js/newsDisplayLoading.js
+++ js/newsDisplayLoading.js
@@ -44,7 +44,11 @@ function LoadArticle(TitleSelector, TitleAppendBefore, TitleAppendAfter,NewsCont
     }
     var ArticleAuthorDisplayCode = 
     '<div class="text-center section-two-item">' +
-        '<img src="' + "../" + XSYD_MemberInfos.portraitBaseURL + mArticleAuthor.portrait + '" class="rounded-circle w-50" />' +
+        (mArticleAuthor.blog == null ? 
+            ('<img src="' +  "../" + XSYD_MemberInfos.portraitBaseURL + mArticleAuthor.portrait + '" class="rounded-circle w-50" />') : 
+            ('<a href="'+ mArticleAuthor.blog +'" target="_blank">' + 
+                '<img src="' +  "../" + XSYD_MemberInfos.portraitBaseURL + mArticleAuthor.portrait + '" class="rounded-circle w-50" />' + 
+            '</a>')) +
         '<div class="mt-3">' +
             '<h3 class="member-name">' + mArticleAuthorLangInfo.displayName + '</h3>' +
             '<p class="member-intro">' + mArticleAuthorLangInfo.role + '</p>' + 
